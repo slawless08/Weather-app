2caaa12e02e5c01cd29417298accbfd56280d5b5
slawless08 <samuel.l.lawless@gmail.com>
2021-10-11 15:21:04 -0400

    Fixed fetch function and document selectors. Added functions to display data and to be able to search. Add to store to local storage.

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,81 @@
 var cityName = document.querySelector(".city");
-var enterBtn = document.querySelector(".search-btn");
+var enterBtn = document.querySelector("#search-btn");
+var displaysResults = document.querySelector("#results");
+var searchHistory = [];
+var historyDiv = document.querySelector("#search-container");
 
 // Run function when search button is clicked
-$(".search-btn").on("click", function(event){
+$(".search-btn").on("click", function (event) {
     event.preventDefault();
-    console.log("This was clicked");
-})
+    let city = cityName.value;
+    getForecast(city);
+});
 
-/* the lines below was to see if I could add an event listener for the enter key after texted was entered into the search box. Does not work :( 
+// Display search history
+function displayHistory () {
+    let history = localStorage.getItem("history");
+    console.log(history.length);
+    // for (var i = 0; i < history.length; i++){
+    //     let createP = document.createElement("p");
+    //     let currentLoop = history[i];
+    //     historyDiv.appendChild(createP);
+    //     createP.setAttribute("id", `${currentLoop}`);
+    //     document.querySelector(`#${currentLoop}`).textContent = `${currentLoop}`
+    // }
+}
+
+// need city name, date, temp, humidity, wind speed, and UV index for 5 days
+async function getForecast(city) {
+    var cityName = city;
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",US&units=imperial&appid=5c1763856a423122de5b32ec2407c6b1"
 
 
-$(".city").on('keydown', function(event){
-    if(event.KeyCode === 13){
-        event.preventDefault();
-        console.log("Enter was pressed!!! ")
-    }
-})
 
-*/
-
-// Api request
-function getForecast(){
-    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",us&appid=58a342ef9741e8a83c016a984c0c2b3c"
-    var cityName = "Columbus";
-
-    fetch(requestUrl)
-        .then(function (response){
-            return response.json();
+    await fetch(requestUrl)
+        .then(function (response) {
+            var weatherData = response.json();
+            return weatherData
         })
-        .then(function (data){
+        .then(function (data) {
             console.log(data);
+            displayForecast(data);
+            searchHistory.splice(0, 0, `${cityName}`);
+            localStorage.setItem('history', searchHistory);
+            displayHistory();
+
         })
 }
 
-// getForecast();
+function displayForecast(weatherData) {
+    let forecastData = {
+        cityName: weatherData.name,
+        feelsLike: weatherData.main.feels_like,
+        humidity: weatherData.main.humidity,
+        temp: weatherData.main.temp,
+        windSpeed: weatherData.wind.speed,
+    }
+
+    function elementCreate(data) {
+        let createP = document.createElement("p");
+        createP.setAttribute("id", `${data}`);
+        displaysResults.appendChild(createP);
+    }
+
+    elementCreate("City");
+    elementCreate("Temp");
+    elementCreate("feels-like");
+    elementCreate("humidity");
+    elementCreate("wind-speed");
+
+    document.querySelector("#City").textContent = `City: ${forecastData.cityName}`;
+    document.querySelector("#Temp").textContent = `Temperature: ${forecastData.temp}`;
+    document.querySelector("#feels-like").textContent = `Feels like: ${forecastData.feelsLike}`;
+    document.querySelector("#humidity").textContent = `Humidity: ${forecastData.humidity}`;
+    document.querySelector("#wind-speed").textContent = `Wind Speed in mph: ${forecastData.windSpeed}`;
+
+
+};
+
+if(localStorage.getItem("history") != null){
+    displayHistory();
+}
